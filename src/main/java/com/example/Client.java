577943c65bad984fbf0ac84deb17970aab77cb37
danielDaleman <danieldaleman@hotmail.com>
2018-04-12 22:18:16 -0500

    servidor con cuadrado

--- src/main/java/com/example/Client.java
+++ src/main/java/com/example/Client.java
@@ -17,7 +17,7 @@ import java.util.logging.Logger;
 public class Client extends Thread{
     
     public static void main(String[] args){
-        for(int i=0;i<20;i++){
+        for(int i=0;i<1;i++){
             Thread hilo = new Client();
             hilo.start();
         }    
@@ -26,8 +26,11 @@ public class Client extends Thread{
     @Override
     public void run(){
         try{
+            System.out.println("PATH = '/'");
             resultado("/");
+            System.out.println("PATH = '/hello'");
             resultado("/hello");            
+            System.out.println("PATH = '/db'");
             resultado("/db");                        
         } catch(MalformedURLException e){
             Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, e);
@@ -37,7 +40,7 @@ public class Client extends Thread{
     }
     
     public void resultado(String tipo) throws MalformedURLException{
-        URL result = new URL("https://aremparcial2.herokuapp.compathHeroku"+tipo);
+        URL result = new URL("https://aremparcial2.herokuapp.com"+tipo);
         try (BufferedReader reader = new BufferedReader(
                 new InputStreamReader(result.openStream()))) {
             String inputLine = null;
